a15ad2ce3a71fa021c0c46a26851c32caac50ba3
Mateo <madiazt@unal.edu.co>
2022-01-28 18:58:20 -0500

    added parsed form data in signin controller

---
+++ dependencies/registerSchemas/sign-in.js
@@ -0,0 +1,13 @@
+const Joi = require("@hapi/joi");
+
+const schemaSignin = Joi.object({
+    username: Joi.string().min(3).max(100).required().trim(),
+    password: Joi.string().min(5). required().trim(),
+    repeat_password: Joi.ref('password'),
+    firstName: Joi.string().max(100).default("").trim(),
+    secondName : Joi.string().max(100).default("").trim(),
+    bio: Joi.string().default("").trim(),
+    picture: Joi.string().default("").trim()
+})
+
+module.exports = schemaSignin;
