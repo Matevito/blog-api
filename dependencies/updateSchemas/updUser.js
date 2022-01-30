9fb86769c05fdbef4108529bde57a5369ee41b9d
Mateo <madiazt@unal.edu.co>
2022-01-30 17:28:38 -0500

    added updateSchemas folder

---
+++ dependencies/updateSchemas/updUser.js
@@ -0,0 +1,10 @@
+const Joi = require("@hapi/joi");
+
+const schemaUpdUser = Joi.object({
+    firstName: Joi.string().max(100).default("").trim(),
+    secondName : Joi.string().max(100).default("").trim(),
+    bio: Joi.string().default("").trim(),
+    picture: Joi.string().default("").trim()
+})
+
+module.exports = schemaUpdUser;
